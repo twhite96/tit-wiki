892874d99aa3398a74f71e15010958b1221914dc
Tiffany <tiffany@tiffanywhite.dev>
2023-03-23 02:45:18 -0400

    Update root note

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     {
       resolve: `gatsby-theme-garden`,
       options: {
-        rootNote: "/index",
+        rootNote: "/readme",
         contentPath: `${__dirname}/..`,
         ignore: [
           "**/_layouts/**",
