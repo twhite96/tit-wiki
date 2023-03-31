801c3e165be0766012611637e9e408add472231f
Tiffany <tiffany@tiffanywhite.dev>
2023-03-31 00:16:11 -0400

    Stuff

--- _layouts/gatsby-ssr.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const headComponents = [
-    <link rel="shortcut icon" href="assests/favicon.png" type="image/x-icon" key="favicon" />
-  ]
-  
-  export const onRenderBody = ({ setHeadComponents }) => {
-    setHeadComponents(headComponents)
-  }
