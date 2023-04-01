ba56157fd696a899c2dc9402684cfa2c0e9698cd
Tiffany <tiffany@tiffanywhite.dev>
2023-04-01 17:17:32 -0400

    Transform wiki links

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -4,7 +4,14 @@ module.exports = {
     title: `tiff's seedlings 🌱`,
   },
   plugins: [
+    `gatsby-transformer-remark`,
     `gatsby-remark-auto-headers-improved`,
+    {
+      resolve: `gatsby-transformer-markdown-references`,
+      options: {
+        types: ["md"], // or ["MarkdownRemark"] (or both)
+      },
+    },
     {
       resolve: `gatsby-plugin-plausible`,
       options: {
